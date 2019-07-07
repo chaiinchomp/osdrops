114f5ff61a4b44f83afc53b749021a31e404af7b
chaiinchomp <chaiinchomp@users.noreply.github.com>
2019-07-06 20:24:02 -0700

    Implement task completion logic

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card v-on="editable ? { click } : {}">
+  <v-card>
     <div class="item-card-header">
       <img
         :class="['item', { editable }, { completed }]"
@@ -33,17 +33,12 @@ export default {
     return {
       title: this.card.title,
       icon: this.card.icon,
+      items: this.card.items,
+      completionCriteria: this.card.criteria,
       completed: 1
     };
   },
   methods: {
-    click() {
-      if (!this.completed) {
-        this.completed = 1;
-      } else {
-        this.completed = 0;
-      }
-    },
     getSrc() {
       return require(`@/assets/img/${this.src}`);
     }
@@ -52,13 +47,29 @@ export default {
     src: function() {
       return this.icon.toLowerCase().replace(/ /g, "_") + ".png";
     }
-  // }, TODO fix this to work with new completion logic
-  // mounted: function() {
-  //   this.completed = this.$store.getters.isUnlocked({
-  //         editable: this.editable,
-  //         item: this.item.id
-  //       }
-  //     );
+  },
+  mounted: function() {
+    let matchCount;
+    if (this.completionCriteria == "all") {
+      matchCount = this.items.length;
+    } else {
+      matchCount = parseInt(this.completionCriteria.substring(4));
+    }
+
+    let matches = 0;
+    for (const item of this.items) {
+      if (this.$store.getters.isUnlocked({
+          editable: this.editable,
+          item: item
+        })) {
+        matches++;
+      }
+      if (matches >= matchCount) {
+        this.completed = 1;
+        return;
+      }
+    }
+    this.completed = 0;
   }
 };
 </script>
